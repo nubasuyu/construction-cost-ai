97160b6659a005dbd301f610896bb62ccdc059c5
Adesina Nurudeen <nubasuyu@gmail.com>
2026-05-24 09:22:18 +0100

    Fixed production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API = ProcessingInstruction.env.REACT_APP_API_URL;
 
 export const predictCost = (data) =>
   axios.post(`${API}/predict`, data);

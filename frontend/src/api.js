aadfd359bcc06988d624dddaf952b8e0970da12a
Adesina Nurudeen <nubasuyu@gmail.com>
2026-05-25 16:21:04 +0100

    corrected url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/* import axios from "axios";
 
 const API = Process.env.REACT_APP_API_URL;
 
@@ -9,4 +9,20 @@ export const saveProject = (data) =>
   axios.post(`${API}/projects`, data);
 
 export const getProjects = () =>
-  axios.get(`${API}/projects`);
+  axios.get(`${API}/projects`); */
+
+
+import axios from "axios";
+
+const API =
+process.env.REACT_APP_API_URL || 
+"http://localhost:5000/api";
+
+export const predictCost = (data) =>
+    axios.post(`${API}/predict`, data);
+
+export const saveProject = (data) =>
+    axios.post(`${API}/projects`, data);
+
+export const getProjects = () =>
+    axios.get(`${API}/projects`);
